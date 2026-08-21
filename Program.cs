b6d0dbd1a5936e5cd1df43afdef9d5f1ee0102c4
Mike Williams <mikey32935@hotmail.com>
2026-08-21 11:14:19 -0400

    added in logos and added in a todo list in programs.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
+//TODO:
+// 1. Add in branding logos
+// 2. Update landing page so this can be added to portfolio
+// 3. Add in a "How to Play" page
+// 4. Redo how tiles are colored for correct letters. (currently tile borders are highlighted. whole tile needs to change color.
+// 5. Add in a "Daily Challenge" mode that is the same for all players each day. (like the original Wordle game)
+// 6. Add in a "Freeplay" mode that allows players to play as many games as they want with random words.
+// 7. Add in a "Hard Mode" that requires players to use correct letters in subsequent guesses.
+
+
+
 using LexiconWASMWordleApp;
 using LexiconWASMWordleApp.Services;
 using Microsoft.AspNetCore.Components.Web;
